fd3b3e99457ae03ff6254bbc3c47105241fcdf29
gedizzletsplay@gmail.com <Duke1@234>
2022-04-07 09:44:00 +0100

    added add() added remove_from_queue() added display_queue() added queue_is_empty()

--- SongQueue.py
+++ SongQueue.py
@@ -1,10 +1,30 @@
 class SongQueue:
 
     def __init__(self):
-        self.queue = []
+        self.queue_list = []
 
-    def add_to_queue(self, song_name):
-        self.queue.append(song_name)
+    async def add(self, song_name, append):
+        self.queue_list.insert(0, song_name) if append else self.queue_list.append(song_name)
 
-    def remove_from_queue(self, index):
-        del(self.queue[index])
+    async def remove_from_queue(self, index):
+        try:
+            song = self.queue_list[index - 1].replace(".mp3", "")
+            del(self.queue_list[index - 1])
+            return song
+        except:
+            return None
+
+    async def display_queue(self, ctx):
+        song_num = 1
+        if self.queue_list:
+            for song in self.queue_list:
+                song.replace(".mp3", "")
+                await ctx.send(f"{song_num}) - {song}")
+                song_num += 1
+        else:
+            await ctx.send("Queue is empty")
+
+    async def queue_is_empty(self):
+        return True if self.queue_list else False
+
+
