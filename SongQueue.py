4456b05907b714ce259d70318d763fddd890b13c
gedizzletsplay@gmail.com <Duke1@234>
2022-04-07 15:09:33 +0100

    added pop() added is_empty() added clear_queue()

--- SongQueue.py
+++ SongQueue.py
@@ -6,6 +6,15 @@ class SongQueue:
     async def add(self, song_name, append):
         self.queue_list.insert(0, song_name) if append else self.queue_list.append(song_name)
 
+    async def pop(self):
+        print(self.queue_list)
+        try:
+            song = self.queue_list[0]
+            del(self.queue_list[0])
+            return song
+        except:
+            return None
+
     async def remove_from_queue(self, index):
         try:
             song = self.queue_list[index - 1].replace(".mp3", "")
@@ -18,13 +27,16 @@ class SongQueue:
         song_num = 1
         if self.queue_list:
             for song in self.queue_list:
-                song.replace(".mp3", "")
+                song = song.replace(".mp3", "")
                 await ctx.send(f"{song_num}) - {song}")
                 song_num += 1
         else:
             await ctx.send("Queue is empty")
 
-    async def queue_is_empty(self):
-        return True if self.queue_list else False
+    async def is_empty(self):
+        return False if self.queue_list else True
+
+    async def clear_queue(self):
+        self.queue_list = []
 
 
